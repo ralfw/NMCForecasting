ae3cf7560357d70bcbdabea224e22c2f82a2c92c
Ralf Westphal <info@ralfw.de>
2019-11-08 17:04:08 +0200

    no weekend delivery when calculating TP

--- src/nmcforecasting/nmcforecasting.tests/ThroughputExtensions_tests.cs
+++ src/nmcforecasting/nmcforecasting.tests/ThroughputExtensions_tests.cs
@@ -6,8 +6,7 @@ namespace nmcforecasting.tests
     public class ThroughputExtensions_tests
     {
         [Fact]
-        public void DailyThroughputs()
-        {
+        public void DailyThroughputs() {
             var historicalData = new[] {
                 new Issue(new DateTime(2019,10,1), new DateTime(2019,10,3), null, "", false), 
                 new Issue(new DateTime(2019,9,30), new DateTime(2019,10,7), null, "", false),
@@ -22,9 +21,21 @@ namespace nmcforecasting.tests
                 new Issue(new DateTime(2019,9,30), new DateTime(2019,10,2), null, "", false),
             };
 
-            var result = historicalData.DailyThroughputs();
+            var result = historicalData.BusinessDayThroughputs();
             
             Assert.Equal(new[]{0,1,2,3,1,4}, result);
+        }
+
+        
+        [Fact]
+        public void Weekend_delivery_not_supported()
+        {
+            var historicalData = new[] {
+                new Issue(new DateTime(2019,10,1), new DateTime(2019,10,5), null, "", false), 
+            };
+
+            var ex = Assert.Throws<InvalidOperationException>(() => historicalData.BusinessDayThroughputs());
+            Assert.True(ex.Message.IndexOf("weekends", StringComparison.Ordinal) >= 0);
         }
     }
 }
--- src/nmcforecasting/nmcforecasting/ThroughputExtensions.cs
+++ src/nmcforecasting/nmcforecasting/ThroughputExtensions.cs
@@ -6,7 +6,7 @@ namespace nmcforecasting
 {
     public static class ThroughputExtensions
     {
-        public static int[] DailyThroughputs(this IEnumerable<Issue> issues) {
+        public static int[] BusinessDayThroughputs(this IEnumerable<Issue> issues) {
             var a_issues = issues.ToArray();
             var calendar = Build_delivery_calendar_without_weekends(a_issues);
             return Compile_daily_throughputs(calendar, a_issues);
@@ -18,18 +18,23 @@ namespace nmcforecasting
             var lastCompletionDate = issues.Max(x => x.Completed);
             return Enumerable.Range(0, (lastCompletionDate - firstStartDate).Days + 1)
                              .Select(offset => firstStartDate.AddDays(offset))
-                             .Where(date => date.DayOfWeek != DayOfWeek.Saturday && 
-                                            date.DayOfWeek != DayOfWeek.Sunday);  
+                             .Where(date => !OnWeekend(date));  
         }
 
         static int[] Compile_daily_throughputs(IEnumerable<DateTime> calendar, Issue[] issues) {
             var throughputs = new Dictionary<DateTime,int>();
             foreach (var date in calendar) throughputs[date] = 0;
-            
-            foreach (var issue in issues)
+
+            foreach (var issue in issues) {
+                if (OnWeekend(issue.Completed)) throw new InvalidOperationException("Throughput calculation does not support issue delivery on weekends!");
                 throughputs[issue.Completed] += 1;
+            }
 
             return throughputs.Values.ToArray();
         }
+
+
+        static bool OnWeekend(DateTime date) 
+            => date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
     }
 }
